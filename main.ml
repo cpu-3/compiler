d6d1864348ae20c752db6973fd59f17a3feb929f
kotapiku <ice08235523@gmail.com>
2018-10-02 20:12:36 +0900

    initial commit

---
+++ main.ml
@@ -0,0 +1,55 @@
+open Parser
+
+let limit = ref 1000
+
+let rec iter n e = (* 最適化処理をくりかえす (caml2html: main_iter) *)
+  Format.eprintf "iteration %d@." n;
+  if n = 0 then e else
+  let e' = Elim.f (ConstFold.f (Inline.f (Assoc.f (Beta.f e)))) in
+  if e = e' then e else
+  iter (n - 1) e'
+
+let lexbuf outchan nml = (* バッファをコンパイルしてチャンネルへ出力する (caml2html: main_lexbuf) *)
+  Id.counter := 0;
+  Typing.extenv := M.empty;
+  Emit.f outchan
+    (RegAlloc.f
+       (Simm.f
+          (Virtual.f
+             (Closure.f
+                (iter !limit
+                   (Alpha.f nml))))))
+
+let lexbuf' outchan stx = lexbuf outchan (KNormal.f (Typing.f stx))
+let lexbuf'' outchan buf = lexbuf' outchan (Parser.exp Lexer.token buf)
+
+let string s = lexbuf'' stdout (Lexing.from_string s) (* 文字列をコンパイルして標準出力に表示する (caml2html: main_string) *)
+
+let file f = (* ファイルをコンパイルしてファイルに出力する (caml2html: main_file) *)
+  let inchan = open_in (f ^ ".ml") in
+  let outchan = open_out (f ^ ".s") in
+  let stx = Parser.exp Lexer.token (Lexing.from_channel inchan) in
+  let nml = KNormal.f (Typing.f stx) in
+  try
+    print_string "Syntax: "; (* Syntax.tの中間結果 *)
+    Syntax.print_t stx;
+    print_newline ();
+    print_string "Normal: "; (* KNormal.tの中間結果 *)
+    KNormal.print_t nml;
+    lexbuf outchan nml;
+    close_in inchan;
+    close_out outchan;
+  with e -> (close_in inchan; close_out outchan; raise e)
+
+let () = (* ここからコンパイラの実行が開始される (caml2html: main_entry) *)
+  let files = ref [] in
+  Arg.parse
+    [("-inline", Arg.Int(fun i -> Inline.threshold := i), "maximum size of functions inlined");
+     ("-iter", Arg.Int(fun i -> limit := i), "maximum number of optimizations iterated")]
+    (fun s -> files := !files @ [s])
+    ("Mitou Min-Caml Compiler (C) Eijiro Sumii\n" ^
+     Printf.sprintf "usage: %s [-inline m] [-iter n] ...filenames without \".ml\"..." Sys.argv.(0));
+  List.iter
+    (fun f -> ignore (file f))
+    !files
+
---
+++ syntax.ml
@@ -0,0 +1,116 @@
+type t = (* MinCamlの構文を表現するデータ型 (caml2html: syntax_t) *)
+  | Unit
+  | Bool of bool
+  | Int of int
+  | Float of float
+  | Not of t
+  | Neg of t
+  | Add of t * t
+  | Sub of t * t
+  | FNeg of t
+  | FAdd of t * t
+  | FSub of t * t
+  | FMul of t * t
+  | FDiv of t * t
+  | Eq of t * t
+  | LE of t * t
+  | If of t * t * t
+  | Let of (Id.t * Type.t) * t * t
+  | Var of Id.t
+  | LetRec of fundef * t
+  | App of t * t list
+  | Tuple of t list
+  | LetTuple of (Id.t * Type.t) list * t * t
+  | Array of t * t
+  | Get of t * t
+  | Put of t * t * t
+and fundef = { name : Id.t * Type.t; args : (Id.t * Type.t) list; body : t }
+
+let rec print_t stx =
+    match stx with
+    | Unit -> print_string "UNIT"
+    | Bool b -> print_string ("BOOL " ^ (string_of_bool b))
+    | Int i -> (print_string "INT ";
+                print_int i)
+    | Float x -> (print_string "FLOAT ";
+                  print_float x)
+    | Not t -> (print_string "NOT ";
+                print_t t)
+    | Neg t -> (print_string "NEG ";
+                print_t t)
+    | FNeg t -> (print_string "FNEG ";
+                 print_t t)
+    | Add (t1, t2) -> (print_t t1;
+                       print_string " + ";
+                       print_t t2)
+    | Sub (t1, t2) -> (print_t t1;
+                   print_string " - ";
+                   print_t t2)
+    | FAdd (t1, t2) -> (print_t t1;
+                   print_string " +. ";
+                   print_t t2)
+    | FSub (t1, t2) -> (print_t t1;
+                   print_string " -. ";
+                   print_t t2)
+    | FMul (t1, t2) -> (print_t t1;
+                   print_string " *. ";
+                   print_t t2)
+    | FDiv (t1, t2) -> (print_t t1;
+                   print_string " /. ";
+                   print_t t2)
+    | Eq (t1, t2) -> (print_t t1;
+                      print_string " = ";
+                      print_t t2)
+    | LE (t1, t2) -> (print_t t1;
+                   print_string " <= ";
+                   print_t t2)
+    | If (t1, t2, t3) -> (print_string "IF ";
+                          print_t t1;
+                          print_string " THEN ";
+                          print_t t2;
+                          print_string " ELSE ";
+                          print_t t3)
+    | Let ((s, _), t2, t3) -> (print_string ("LET " ^ s ^ " = ");
+                               print_t t2;
+                               print_string " IN ";
+                               print_t t3)
+    | Var s -> print_string ("VAR " ^ s)
+    | LetRec (fd, t) -> (print_string ("LET REC " ^ (fst fd.name) ^ "(" ^ (fst (List.hd fd.args)));
+                         List.iter (fun (s, _) ->
+                            print_string (", " ^ s)) (List.tl fd.args);
+                         print_string ") = ";
+                         print_t fd.body;
+                         print_string " IN ";
+                         print_t t)
+    | App (t1, t2::ts) -> (print_t t1;
+                           print_string "(";
+                           print_t t2;
+                           List.iter (fun t' ->
+                               print_string ", ";
+                               print_t t') ts;
+                           print_string ")")
+    | Tuple (t::ts) -> (print_string "[";
+                        print_t t;
+                        List.iter (fun t' ->
+                            print_string ", ";
+                            print_t t') ts;
+                        print_string "]")
+    | LetTuple (s::ss, t1, t2) -> (print_string ("LET (" ^ (fst s));
+                                   List.iter (fun (s', _) ->
+                                       print_string (", " ^ s')) ss;
+                                   print_string ") = ";
+                                   print_t t1;
+                                   print_string " IN ";
+                                   print_t t2)
+    | Array (t1, t2) -> (print_string "ARRAY_CREATE ";
+                         print_t t1;
+                         print_t t2)
+    | Get (t1, t2) -> (print_t t1;
+                       print_string " . (";
+                       print_t t2;
+                       print_string ")")
+    | Put (t1, t2, t3) -> (print_t t1;
+                           print_string " . (";
+                           print_t t2;
+                           print_string ") <-";
+                           print_t t3)
